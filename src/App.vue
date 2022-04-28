4b44060076d466d6d8b9f15255bd663ed2268712
samuele <spadaro.samuele@gmail.com>
2022-04-28 15:38:16 +0200

    importazione di bootstrap, fontaweson e le img

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    
+    <header></header>
+    <main></main>
+    <footer></footer>
   </div>
 </template>
 
@@ -16,12 +18,17 @@ export default {
 </script>
 
 <style lang="scss">
+// reset
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
