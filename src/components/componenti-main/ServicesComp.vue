6e1961db2f21ec894b429ec9534e17836f14c98e
samuele <spadaro.samuele@gmail.com>
2022-04-29 10:19:25 +0200

    sezione servizi finita

--- src/components/componenti-main/ServicesComp.vue
+++ src/components/componenti-main/ServicesComp.vue
@@ -2,23 +2,34 @@
     <div class="bg_color py_5">
         <!--inserire contenuto componente-->
         <div class="containers">
-            <h6 class="text-uppercase">our business areas</h6>
+            <h6 class="text-uppercase c-fountain-blue">our business areas</h6>
             <h2 class="fs-1 fw-bold py-3 text-light">Excellence in <span class="text-custom">Services</span></h2>
             <div class="d-flex justify-content-between">
                 <div class="col-8">
-                    <p>We are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and renowed professional. these are some options that you can hire</p>
+                    <p class="c-silver-sand">We are leaders in providing consultancy services with  a set of cutting-edge
+                        technologies and a team of experienced and renowed professional. these are some options that you
+                        can hire</p>
                 </div>
 
                 <div>
-                    <button type="button" class="btn text-uppercase text-light fw-bold border-blue-lagoon px-3">see all</button>
+                    <button type="button" class="btn text-uppercase text-light fw-bold border-blue-lagoon px-3">see
+                        all</button>
                 </div>
 
             </div>
 
-            <div class=" d-flex justify-content-between">
-                <div v-for="(element, index) in numberUtility" :key="index" class="text-light d-flex flex-column mt-5">
-                    <span class="c-fountain-blue fw-bold fs-3">{{element.numero}}</span>
-                    <span class="fw-bold fs-5">{{element.nome}}</span>
+            <div class=" d-flex justify-content-between flex-wrap">
+                <div v-for="(element, index) in Services" :key="index" class="text-light bg_color_2 box mt-4 rounded">
+                    <div class="box-2">
+                        <div class="mb-3 d-flex justify-content-between pt-5">
+                            <span class="c-blue-lagoon fs-2"><i :class="`${element.icona}`"></i></span>
+                            <span class="c-blue-lagoon fs-4"><i :class="`${element.freccia}`"></i></span>
+                        </div>
+                        <h6 class="text-light fw-bold fs-5">{{element.servizio}}</h6>
+                        <p class="c-silver-sand pb-5">{{element.descrizione}}</p>
+                    </div>
+
+
                 </div>
             </div>
         </div>
@@ -84,16 +95,20 @@
         background-color: $woodsmoke;
     }
 
+    .bg_color_2 {
+        background-color: $abbey-01;
+    }
+
     .containers {
         width: 80%;
         margin: 0 auto;
     }
 
-    h6 {
+    .c-blue-lagoon {
         color: $blue-lagoon;
     }
 
-    p {
+    .c-silver-sand {
         color: $silver-sand;
     }
 
@@ -113,5 +128,13 @@
 
     .border-blue-lagoon {
         border: 1px solid $blue-lagoon;
+    }
+
+    .box-2 {
+        width: 75%;
+        margin: 0 auto;
+    }
+    .box{
+        width: calc((100% / 3) - 15px);
     }
 </style>
